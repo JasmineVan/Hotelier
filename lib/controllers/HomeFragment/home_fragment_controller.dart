e7c78cae86d1379b483370a7638af47215b74339
TrinhThuong2001 <88668726+JasmineVan@users.noreply.github.com>
2023-12-23 23:49:05 +0700

    Add Nearby and Popular tabview, explore promotion

--- lib/controllers/HomeFragment/home_fragment_controller.dart
+++ lib/controllers/HomeFragment/home_fragment_controller.dart
@@ -3,11 +3,38 @@ import 'package:hotelier/data/models/hotel.dart';
 
 class HomeFragmentController extends GetxController{
 
-  List<Hotel> hotels = [
+  List<String> dropdownValue = [
+    'District 1',
+    'District 2',
+    'District 3',
+    'District 4'
+  ];
+
+  List<Hotel> hotHotels = [
     Hotel(pictureURL: 'assets/images/bg (1).jpg', rating: 1.0),
     Hotel(pictureURL: 'assets/images/bg (2).jpg', rating: 2.0),
     Hotel(pictureURL: 'assets/images/bg (3).jpg', rating: 3.0),
     Hotel(pictureURL: 'assets/images/bg (4).jpg', rating: 4.0),
     Hotel(pictureURL: 'assets/images/bg (5).jpg', rating: 5.0),
   ];
+
+  List<Hotel> nearbyHotels = [
+    Hotel(pictureURL: 'assets/images/bg (6).jpg', rating: 1.0),
+    Hotel(pictureURL: 'assets/images/bg (7).jpg', rating: 2.0),
+    Hotel(pictureURL: 'assets/images/bg (8).jpg', rating: 3.0),
+    Hotel(pictureURL: 'assets/images/bg (9).jpg', rating: 4.0),
+    Hotel(pictureURL: 'assets/images/bg (10).jpg', rating: 5.0),
+  ];
+
+  List<Hotel> popularHotels = [
+    Hotel(pictureURL: 'assets/images/bg (11).jpg', rating: 1.0),
+    Hotel(pictureURL: 'assets/images/bg (12).jpg', rating: 2.0),
+    Hotel(pictureURL: 'assets/images/bg (13).jpg', rating: 3.0),
+    Hotel(pictureURL: 'assets/images/bg (14).jpg', rating: 4.0),
+    Hotel(pictureURL: 'assets/images/bg (15).jpg', rating: 5.0),
+  ];
+
+  String getDefaultValue(){
+    return dropdownValue.first;
+  }
 }
--- lib/views/HomeFragment/home_fragment.dart
+++ lib/views/HomeFragment/home_fragment.dart
@@ -3,13 +3,6 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:hotelier/controllers/HomeFragment/home_fragment_controller.dart';
 
-const List<String> dropdownValue = [
-  'District 1',
-  'District 2',
-  'District 3',
-  'District 4'
-];
-
 class HomeFragment extends StatefulWidget {
   const HomeFragment({Key? key}) : super(key: key);
 
@@ -18,13 +11,14 @@ class HomeFragment extends StatefulWidget {
 }
 
 class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderStateMixin{
-  String defaultValue = dropdownValue.first;
-  TabController? tabController;
   final controller = Get.put(HomeFragmentController());
+  String? defaultValue;
+  TabController? tabController;
 
   @override
   void initState() {
     tabController = TabController(length: 3, vsync: this);
+    defaultValue = controller.getDefaultValue();
     super.initState();
   }
 
@@ -34,8 +28,10 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
     double maxHeight = MediaQuery.sizeOf(context).height;
 
     return Scaffold(
+      backgroundColor: Colors.transparent,
       body: SingleChildScrollView(
         child: Container(
+          color: Colors.transparent,
           margin: EdgeInsets.only(
               left: maxWidth / 20,
               right: maxWidth / 20,
@@ -43,6 +39,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
               bottom: maxWidth / 10),
           child: Column(
             children: [
+              // TODO: This is Search bar
               SizedBox(
                 width: maxWidth * 90 / 100,
                 height: maxHeight * 1 / 10,
@@ -62,6 +59,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                   maxLength: 50,
                 ),
               ),
+              // TODO: This Main carousel
               ClipRRect(
                 borderRadius: const BorderRadius.all(Radius.circular(16.0)),
                 child: Container(
@@ -165,6 +163,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                   ),
                 ),
               ),
+              // TODO: This is Dropdown button and locale
               Container(
                 margin: EdgeInsets.symmetric(vertical: maxWidth / 40),
                 child: Row(
@@ -172,7 +171,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                   children: [
                     DropdownButton(
                       value: defaultValue,
-                      items: dropdownValue.map((String value) {
+                      items: controller.dropdownValue.map((String value) {
                         return DropdownMenuItem(
                           value: value,
                           child: Text(value),
@@ -225,6 +224,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                   ],
                 ),
               ),
+              // TODO: This is Tab bar
               TabBar(
                 dividerColor: Colors.transparent,
                 indicatorColor: Colors.grey,
@@ -269,6 +269,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                 controller: tabController,
                 indicatorSize: TabBarIndicatorSize.tab,
               ),
+              // TODO: This is Tab view
               Container(
                 margin: EdgeInsets.symmetric(vertical: maxWidth / 40),
                 // color: Colors.yellow,
@@ -279,7 +280,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                     children: [
                       ListView.builder(
                         scrollDirection: Axis.horizontal,
-                          itemCount: controller.hotels.length,
+                          itemCount: controller.hotHotels.length,
                           itemBuilder: (context, index) {
                             return Stack(
                               children: [
@@ -292,7 +293,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                                       height: maxHeight/4,
                                       decoration: BoxDecoration(
                                           image: DecorationImage(
-                                            image: AssetImage(controller.hotels.elementAt(index).pictureURL),
+                                            image: AssetImage(controller.hotHotels.elementAt(index).pictureURL),
                                             fit: BoxFit.cover,
                                           )
                                       ),
@@ -316,7 +317,7 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                                           mainAxisAlignment: MainAxisAlignment.center,
                                           children: [
                                             Text(
-                                              controller.hotels.elementAt(index).rating.toString(),
+                                              controller.hotHotels.elementAt(index).rating.toString(),
                                               style: GoogleFonts.montserrat(
                                                   textStyle: const TextStyle(
                                                     fontWeight: FontWeight.bold,
@@ -338,20 +339,223 @@ class _HomeFragmentState extends State<HomeFragment> with SingleTickerProviderSt
                             );
                           },
                       ),
-                      const Center(
-                        child: Text(
-                          'Screen 2',
-                        ),
+                      ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: controller.nearbyHotels.length,
+                        itemBuilder: (context, index) {
+                          return Stack(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: maxHeight/40),
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                  child: Container(
+                                    width: (maxWidth - (maxHeight/40)*4)/3,
+                                    height: maxHeight/4,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(controller.nearbyHotels.elementAt(index).pictureURL),
+                                          fit: BoxFit.cover,
+                                        )
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Container(
+                                  margin: const EdgeInsets.only(top: 8.0, left: 8.0),
+                                  child: ClipRRect(
+                                    borderRadius:
+                                    const BorderRadius.all(Radius.circular(12.0)),
+                                    child: Container(
+                                      width: maxWidth * 1 / 8,
+                                      height: maxHeight * 1 / 30,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.red,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            controller.nearbyHotels.elementAt(index).rating.toString(),
+                                            style: GoogleFonts.montserrat(
+                                                textStyle: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.white,
+                                                  fontSize: 12.0,
+                                                )),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          Icon(Icons.star,
+                                              color: Colors.white.withOpacity(0.9),
+                                              size: 12.0),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        },
                       ),
-                      const Center(
-                        child: Text(
-                          'Screen 3',
-                        ),
+                      ListView.builder(
+                        scrollDirection: Axis.horizontal,
+                        itemCount: controller.popularHotels.length,
+                        itemBuilder: (context, index) {
+                          return Stack(
+                            children: [
+                              Container(
+                                margin: EdgeInsets.only(right: maxHeight/40),
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                                  child: Container(
+                                    width: (maxWidth - (maxHeight/40)*4)/3,
+                                    height: maxHeight/4,
+                                    decoration: BoxDecoration(
+                                        image: DecorationImage(
+                                          image: AssetImage(controller.popularHotels.elementAt(index).pictureURL),
+                                          fit: BoxFit.cover,
+                                        )
+                                    ),
+                                  ),
+                                ),
+                              ),
+                              Align(
+                                alignment: Alignment.topRight,
+                                child: Container(
+                                  margin: const EdgeInsets.only(top: 8.0, left: 8.0),
+                                  child: ClipRRect(
+                                    borderRadius:
+                                    const BorderRadius.all(Radius.circular(12.0)),
+                                    child: Container(
+                                      width: maxWidth * 1 / 8,
+                                      height: maxHeight * 1 / 30,
+                                      decoration: const BoxDecoration(
+                                        color: Colors.red,
+                                      ),
+                                      child: Row(
+                                        mainAxisAlignment: MainAxisAlignment.center,
+                                        children: [
+                                          Text(
+                                            controller.popularHotels.elementAt(index).rating.toString(),
+                                            style: GoogleFonts.montserrat(
+                                                textStyle: const TextStyle(
+                                                  fontWeight: FontWeight.bold,
+                                                  color: Colors.white,
+                                                  fontSize: 12.0,
+                                                )),
+                                            textAlign: TextAlign.center,
+                                          ),
+                                          Icon(Icons.star,
+                                              color: Colors.white.withOpacity(0.9),
+                                              size: 12.0),
+                                        ],
+                                      ),
+                                    ),
+                                  ),
+                                ),
+                              )
+                            ],
+                          );
+                        },
                       ),
                     ],
                   ),
                 ),
               ),
+              // TODO: Recently booked
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Explore Our Promotion",
+                  style: GoogleFonts.montserrat(
+                      textStyle: const TextStyle(
+                        fontWeight: FontWeight.bold,
+                        color: Colors.black,
+                        fontSize: 16.0,
+                      )),
+                  textAlign: TextAlign.center,
+                ),
+              ),
+              // TODO: This is Subordinate carousel
+              Container(
+                margin: EdgeInsets.symmetric(vertical: maxWidth / 40),
+                child: ClipRRect(
+                  borderRadius: const BorderRadius.all(Radius.circular(16.0)),
+                  child: Container(
+                    padding: EdgeInsets.all(maxWidth / 20),
+                    width: maxWidth * 9 / 10,
+                    height: maxHeight / 5,
+                    decoration: const BoxDecoration(
+                      image: DecorationImage(
+                          image: AssetImage('assets/images/bg (2).jpg'),
+                          fit: BoxFit.cover,
+                          opacity: 0.8),
+                    ),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.center,
+                          child: ClipRRect(
+                            borderRadius:
+                            const BorderRadius.all(Radius.circular(16.0)),
+                            child: Container(
+                              padding: const EdgeInsets.all(8.0),
+                              color: Colors.grey.withOpacity(0.5),
+                              width: maxWidth * 10 / 20,
+                              height: maxHeight / 10,
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    "SALE UP TP 50%",
+                                    style: GoogleFonts.montserrat(
+                                        textStyle: const TextStyle(
+                                          fontWeight: FontWeight.bold,
+                                          color: Colors.white,
+                                          fontSize: 16.0,
+                                        )),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                  Text(
+                                    "Book your first stay",
+                                    style: GoogleFonts.montserrat(
+                                        textStyle: const TextStyle(
+                                          color: Colors.white,
+                                          fontSize: 16.0,
+                                        )),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              // TODO: See all offers button
+              SizedBox(
+                width: maxWidth *2/3,
+                height: maxHeight / 24,
+                child: ElevatedButton(
+                  onPressed: () {},
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: Colors.white,
+                    side: const BorderSide(color: Colors.black),
+                  ),
+                  child: const Text(
+                    'See all offers',
+                    style: TextStyle(color: Colors.black),
+                  ),
+                ),
+              ),
+              // TODO: Recently booked
             ],
           ),
         ),
